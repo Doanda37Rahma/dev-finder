f3712dcb3d79ca846f53fe884f7bd134b24a1d6f
Doanda37Rahma <ddoanda@gmail.com>
2023-03-30 17:48:10 +0700

    feat: get users by query from github API and show

---
+++ app/src/main/java/com/dicoding/doanda/devfinder/ApiService.kt
@@ -0,0 +1,12 @@
+package com.dicoding.doanda.devfinder
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+//    https://api.github.com/search/users?q=doanda
+    @GET("search/users")
+    fun searchUsers(
+        @Query("q") q: String
+    ): Call<GithubSearchResponse>
+}
--- app/src/main/java/com/dicoding/doanda/devfinder/MainActivity.kt
+++ app/src/main/java/com/dicoding/doanda/devfinder/MainActivity.kt
@@ -2,9 +2,14 @@ package com.dicoding.doanda.devfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.doanda.devfinder.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,18 +29,71 @@ class MainActivity : AppCompatActivity() {
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.rvUsers.addItemDecoration(itemDecoration)
 
-        val listUsersDemo = listOf<User>(
-            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
-            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
-            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
-            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
-            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
-            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
-            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
-        )
+//        val listUsersDemo = listOf<User>(
+//            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
+//            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
+//            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
+//            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
+//            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
+//            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
+//            User("https://avatars.githubusercontent.com/u/69817149?v=4", "doanda"),
+//        )
+//        val adapter = UserAdapter(listUsersDemo)
+//        binding.rvUsers.adapter = adapter
 
-        val adapter = UserAdapter(listUsersDemo)
+        findUsers()
+    }
 
+    private fun showLoading(isLoading: Boolean) {
+
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+
+    private fun findUsers() {
+        showLoading(true)
+//        TODO("Implement Search Feature")
+        val client = ApiConfig.getApiService().searchUsers("doanda")
+        client.enqueue(object: Callback<GithubSearchResponse>{
+            override fun onResponse(
+                call: Call<GithubSearchResponse>,
+                response: Response<GithubSearchResponse>
+            ) {
+                showLoading(false)
+                if(response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        setUserListData(responseBody.items)
+                    }
+                } else {
+                    Log.e(TAG, "onResponseIsUnsuccessful: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<GithubSearchResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+
+    }
+
+    private fun setUserListData(items: List<ItemsItem?>?) {
+        val listUsers = ArrayList<User>()
+        if (items != null) {
+            for (item in items) {
+                val avatar = item?.avatarUrl ?: getString(R.string.default_avatar_url)
+                val name = item?.login ?: getString(R.string.default_username)
+                val user = User(avatar, name)
+                listUsers.add(user)
+            }
+        }
+
+        val adapter = UserAdapter(listUsers)
         binding.rvUsers.adapter = adapter
     }
+
 }
