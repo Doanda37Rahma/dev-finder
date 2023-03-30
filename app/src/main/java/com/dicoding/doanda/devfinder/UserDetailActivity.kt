5805ed1fdb69c295ba1f26985913822aa47c3252
Doanda37Rahma <ddoanda@gmail.com>
2023-03-31 04:41:47 +0700

    feat: add layouts for followers and following list in user detail

--- app/src/main/java/com/dicoding/doanda/devfinder/UserDetailActivity.kt
+++ app/src/main/java/com/dicoding/doanda/devfinder/UserDetailActivity.kt
@@ -3,17 +3,26 @@ package com.dicoding.doanda.devfinder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.dicoding.doanda.devfinder.databinding.ActivityUserDetailBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class UserDetailActivity : AppCompatActivity() {
 
     companion object {
         val EXTRA_USER = "extra_user"
 
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_followers,
+            R.string.tab_following,
+        )
     }
 
     private lateinit var binding: ActivityUserDetailBinding
@@ -23,7 +32,15 @@ class UserDetailActivity : AppCompatActivity() {
         binding = ActivityUserDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
 
+        supportActionBar?.elevation = 0f
 
         val user = if (Build.VERSION.SDK_INT >= 33) {
             intent.getParcelableExtra<User>(EXTRA_USER, User::class.java)
